fa46bf4eb725b7b400a8e8a7524cd5272ec84660
CloudyChris <a.c.pufu@gmail.com>
2025-06-23 20:34:05 +0300

    added Dungeon Crawl example

--- examples/dungeon_crawl_game/main.py
+++ examples/dungeon_crawl_game/main.py
@@ -1,40 +1,81 @@
-grid = '''##########
-#& #     #
-#  #     #
-#X #     #
-#      X@#
-##########'''
-
-wall = "#"
-space = " "
-player = "&"
-goal = "@"
-trap = "X"
+import os
 
 class Character:
-    x = -1
-    y= -1
-    is_gameover = False
-    is_win = False
+    # grid
+    grid = ""
     grid_w = 0
     grid_h = 0
     comp_grid = []
-    
+
+    # characters
+    wall = "#"
+    space = " "
+    player = "&"
+    goal = "@"
+    trap = "X"
+
+    # position
+    x = -1
+    y= -1
+
+    # state
+    is_gameover = False
+    is_win = False
+
+    def clear(self):
+        match os.name:
+            case "nt":
+                os.system("cls")
+            case _:
+                os.system("clear")
+
+    def menu(self):
+        maps_dir = "./maps"
+        maps = []
+        for root, dirs, files in os.walk(maps_dir):
+            for _file in files:
+                file_handler = open(maps_dir+"/"+_file, "r")
+                maps.append(file_handler.read())
+                file_handler.close()
+                print(len(maps)-1)
+                print(maps[len(maps)-1])
+                print("\n")
+
+        while self.grid == "":
+            cmd = input("Select map: ")
+            try:
+                cmd = int(cmd)
+                self.grid = maps[cmd]
+            except:
+                print("Please enter a valid number!\n")
+
     def main(self):
+        self.clear()
+        self.menu()
+        self.clear()
         self.setup()
+
         while not self.is_gameover:
             self.render()
             cmd = input("Move: ")
             if self.move(cmd):
                 return True
-                
+            self.clear()
+
     def setup(self):
-        grid_arr = grid.split("\n")
+        grid_tmp = self.grid.split("\n")
+        grid_arr = []
+        for i in grid_tmp:
+            if i.startswith("#") and i.endswith("#"):
+                grid_arr.append(i)
+
         try:
             self.grid_h = len(grid_arr)
             self.grid_w = len(grid_arr[0])
         except:
             print("Grid bad")
+            quit()
+
         self.is_gameover = False
         self.is_win = False
         self.comp_grid = []
@@ -46,8 +87,8 @@ class Character:
 
         for y in range(self.grid_h):
             for x in range(self.grid_w):
-                if self.comp_grid[y][x] == player:
-                    self.comp_grid[y][x] = space
+                if self.comp_grid[y][x] == self.player:
+                    self.comp_grid[y][x] = self.space
                     self.x = x
                     self.y = y
                     return
@@ -75,21 +116,21 @@ class Character:
                 pass
 
         if cx >= 0 and cx <= self.grid_w and cy >= 0 and cy <= self.grid_h:
-            if self.comp_grid[cy][cx] == wall:
+            if self.comp_grid[cy][cx] == self.wall:
                 return False
-            elif self.comp_grid[cy][cx] == trap:
+            elif self.comp_grid[cy][cx] == self.trap:
                 self.is_gameover = True
                 return self.end_game()
-            elif self.comp_grid[cy][cx] == goal:
+            elif self.comp_grid[cy][cx] == self.goal:
                 self.is_win = True
                 self.is_gameover = True
                 return self.end_game()
-            
+
             self.x = cx
             self.y = cy
-        
+
         return False
-    
+
     def move(self, cmd):
         cmd = cmd.split(" ")
 
@@ -106,18 +147,19 @@ class Character:
                 except:
                     if self._move(move_cmd, move_amount):
                         return True
-            
+
             move_cmd = ""
             move_amount = 1
 
             if token in ["up", "down", "left", "right", "quit"]:
                 move_cmd = token
-        
+
         if move_cmd != "":
             if self._move(move_cmd, move_amount):
                 return True
-                
+
     def end_game(self):
+        self.clear()
         if self.is_win:
             print("##########")
             print("#        #")
@@ -135,13 +177,14 @@ class Character:
         repl = ""
         while repl != "y" or repl != "n":
             repl = input("Play again(y/n)? ")
+            print("\n")
             if repl == "y":
                 return True
             elif repl == "n":
                 quit()
             else:
                 print("Please answer y or n")
-    
+
     def render(self):
         render_target = ""
         for y in range(self.grid_h):
@@ -151,7 +194,7 @@ class Character:
                 else:
                     render_target += self.comp_grid[y][x]
             render_target += "\n"
-    
+
         print(render_target)
         print("\n")
 
